c1a1844988d3c46befc38b2e4fb4e783a3e57a00
Joshua Evans <jevangelistag@toluca.tecnm.mx>
2024-04-04 09:46:02 -0600

    Add Script SQL

---
+++ CreacionDB.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE Encuestas (
+    Id INT PRIMARY KEY IDENTITY,
+    Respuesta1 NVARCHAR(MAX),
+    Respuesta2 NVARCHAR(MAX),
+    Respuesta3 NVARCHAR(MAX),
+    Respuesta4 NVARCHAR(MAX),
+    Respuesta5 NVARCHAR(MAX),
+    Respuesta5_1 NVARCHAR(MAX),
+    Respuesta5_2 NVARCHAR(MAX),
+    Respuesta6 NVARCHAR(MAX),
+    Respuesta7 NVARCHAR(MAX),
+    Respuesta8 NVARCHAR(MAX)
+);
